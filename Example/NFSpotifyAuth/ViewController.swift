8fd4a9fe530384d32a22912e19bd749534e67db9
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-04-09 17:34:46 +0800

    Update pods and deployment target

--- Example/NFSpotifyAuth/ViewController.swift
+++ Example/NFSpotifyAuth/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import NFSpotifyAuth
 
-class ViewController: UIViewController {
+let SpotifyClientID = "c435e1b830ed4aee9260e8d0e319c7cd"
+let SpotifyClientSecret = "cee1df07206048a7b665977dd74301c6"
+let SpotifyCallbackURI = "http://api.domain/callback"
+let SpotifyTokenRefreshServiceURL = "http://api.domain/callback/refresh"
+let SpotifyTokenSwapServiceURL = "http://api.domain/callback/swap"
 
+class ViewController: UIViewController, NFSpotifyLoginViewDelegate {
+
+    private var loginView: NFSpotifyLoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NFSpotifyOAuth.shared.set(clientId: SpotifyClientID, clientSecret: SpotifyClientSecret, redirectURI: SpotifyCallbackURI)
+
+        let rectFrame = CGRect(x: 30, y: 80, width: view.frame.width - 60, height: 400)
+        loginView = NFSpotifyLoginView(frame: rectFrame, scopes: NFSpotifyAvailableScopes, delegate: self)
+        view.addSubview(loginView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loginView.show()
     }
 
 }
 
+// MARK: - NFSpotifyLoginViewDelegate
+
+extension ViewController {
+    
+    func spotifyLoginView(_ view: NFSpotifyLoginView, didFailWithError error: Error?) {
+        
+        print("err: \(error)")
+    }
+    
+    func spotifyLoginView(_ view: NFSpotifyLoginView, didLoginWithTokenObject tObject: NFSpotifyToken) {
+        
+        print("didLoginWithTokenObject: \(tObject)")
+
+    }
+
+}
